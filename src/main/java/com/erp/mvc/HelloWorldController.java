e5de0e804f7345302c0f464e34fadc8b68600ef2
yangxue <yangxue088@gmail.com>
2015-07-24 12:54:48 +0800

    完整的springmvc+sitemesh+boostrap工程

---
+++ src/main/java/com/erp/mvc/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.erp.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping("/index")
+    public String helloWorld() {
+        return "index";
+    }
+}
